12882349b1b854d707b90fb28abd9f5a02e1e881
juan eugenio <leuviah77@gmail.com>
2023-06-30 17:07:02 +0200

    added soundclick to the Navigation Links with the library use-sound

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,58 @@
 /** @format */
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import useSound from "use-sound";
+import soundClick from "../assets/bubble-click.wav";
+
+// See this, for Info about useSound: https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/#rising-pitch-11
 
 function Navbar() {
+	const [isHovering, setIsHovering] = useState(false);
+	const [play] = useSound(soundClick, { volume: 0.1 });
+
+	console.log("%c 👉 Line-12", "font-size:13px; background:#993441; color:#ffb8b1;", isHovering);
 	return (
 		<nav className="menubar">
 			<img className="nav-picture" src="src/assets/profile-pic.png" alt="profile picture" />
-			<Link to="/" className="menulink">
+			<Link
+				to="/"
+				className="menulink"
+				onMouseEnter={() => {
+					setIsHovering(true);
+					play();
+				}}
+			>
 				home
 			</Link>
-			<Link to="/projects" className="menulink">
+			<Link
+				to="/projects"
+				className="menulink"
+				onMouseEnter={() => {
+					setIsHovering(true);
+					play();
+				}}
+			>
 				projects
 			</Link>
-			<Link to="/about" className="menulink">
+			<Link
+				to="/about"
+				className="menulink"
+				onMouseEnter={() => {
+					setIsHovering(true);
+					play();
+				}}
+			>
 				about
 			</Link>
-			<Link to="/contact" className="menulink">
+			<Link
+				to="/contact"
+				className="menulink"
+				onMouseEnter={() => {
+					setIsHovering(true);
+					play();
+				}}
+			>
 				contact
 			</Link>
 		</nav>
