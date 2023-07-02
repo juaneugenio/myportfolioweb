3ec3621c5c5b94b99717080015428cde52a57c2f
juan eugenio <leuviah77@gmail.com>
2023-07-02 22:21:53 +0200

    Finish content in Homepage and styled with clamp the quote and the text for the hole app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,28 +12,27 @@ function Home({ variants }) {
 			transition={{ ease: "linear", duration: 0.5, x: { duration: 1 } }}
 		>
 			<blockquote className="quote">
-				<q>Debugging the present to develop the future</q>
+				👋🏼 Hello! <br />I am <strong>Juan</strong>, welcome to my Portfolio
+				{/* <q>Debugging the present to develop the future</q> */}
 			</blockquote>
 			<div className="home-content">
 				<p>
-					👋🏼 Hi there and welcome to my Web Portfolio! My name is <strong>Juan</strong> , and it is a pleasure to have
-					you here. Allow me to share my passion for web development with you.
+					My journey as a developer began back in 2021, when I dived into the exciting realm of the MERN stack. Since
+					then, I&apos;ve been consistently improving my skills through both front-end and back-end projects, focusing
+					on JavaScript, React, RESTful APIs, CRUD operatins and more.
 				</p>
 				<p>
-					My journey in this field began in 2021 as a developer specializing in the MERN stack. Since then, I have been
-					consistently working on front and back end projects to enhance my technical skills, including JavaScript,
-					React, Restful APIs, and more. Please, Feel free to explore my projects to gain a better understanding of my
-					work.
+					However, being a proficient professional extends beyond technical expertise alone. During my career as a
+					Spanish teacher, I developed the ability to listen carefully to the learning needs of my students and to offer
+					effective solutions to the challenges they face in their learning process. I am confident that the combination
+					of my technical and interpersonal skills will help me to work effectively with both clients and development
+					teams.
 				</p>
 				<p>
-					However, being a proficient professional goes beyond technical expertise. Drawing from my experience as a
-					Spanish teacher, I have come to realize that interpersonal skills are equally vital. These skills allow
-					individuals to stand out as more than just average workers and transform into reliable collaborators.
-				</p>
-				<p>
-					Currently, I am in the process of transitioning my career to web development because I genuinely love it. I am
-					thrilled about the prospect of joining a team of colleagues who can help me grow not only as a developer but
-					also as a professional. The opportunity to evolve alongside them is truly exciting to me.
+					My immediate objective as a career changer is to secure a full-time position in web development, ideally
+					within a dynamic team that appreciates my drive and dedication. <br />
+					If you would like to learn more about how my qualifications can contribute to your organization, I welcome the
+					opportunity to connect and discuss further.
 				</p>
 			</div>
 		</motion.section>
