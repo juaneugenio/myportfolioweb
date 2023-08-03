fd1fa9a67b271efacdc1f19c363a5d07d7672607
juan eugenio <leuviah77@gmail.com>
2023-08-03 13:27:41 +0200

    moved main into root folder

--- main.jsx
+++ main.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import router from "./src/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
