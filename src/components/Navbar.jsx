d860781f073db1f3f3b5ca85fb7e0a3d39b7a85f
juaneugenio <leuviah77@gmail.com>
2024-11-12 17:09:05 +0100

    Refactored Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 /** @format */
 
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import useSound from "use-sound";
 import soundClick from "../assets/bubble-click.wav";
 import profilPict from "../assets/profile-pic.png";
@@ -9,53 +8,27 @@ import profilPict from "../assets/profile-pic.png";
 // See for Info about useSound: https://www.joshwcomeau.com/react/announcing-use-sound-react-hook/#rising-pitch-11
 
 function Navbar() {
-	const [isHovering, setIsHovering] = useState(false);
 	const [play] = useSound(soundClick, { volume: 0.1 });
 
-	// console.log("%c 👉 Line-12", isHovering);
+	const handleMouseEnter = () => {
+		play();
+	};
+
+	const menuLinks = [
+		{ name: "home", path: "/" },
+		{ name: "projects", path: "/projects" },
+		{ name: "about", path: "/about" },
+		{ name: "contact", path: "/contact" },
+	];
+
 	return (
 		<nav className="menubar">
 			<img className="nav-picture" src={profilPict} alt="profile picture" />
-			<Link
-				to="/"
-				className="menulink"
-				onMouseEnter={() => {
-					setIsHovering(true);
-					play();
-				}}
-			>
-				home
-			</Link>
-			<Link
-				to="/projects"
-				className="menulink"
-				onMouseEnter={() => {
-					setIsHovering(true);
-					play();
-				}}
-			>
-				projects
-			</Link>
-			<Link
-				to="/about"
-				className="menulink"
-				onMouseEnter={() => {
-					setIsHovering(true);
-					play();
-				}}
-			>
-				about
-			</Link>
-			<Link
-				to="/contact"
-				className="menulink"
-				onMouseEnter={() => {
-					setIsHovering(true);
-					play();
-				}}
-			>
-				contact
-			</Link>
+			{menuLinks.map((item) => (
+				<Link key={item.path} to={item.path} className="menulink" onMouseEnter={handleMouseEnter}>
+					{item.name}
+				</Link>
+			))}
 		</nav>
 	);
 }
